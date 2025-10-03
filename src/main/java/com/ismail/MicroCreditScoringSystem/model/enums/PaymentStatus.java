3e3264a19e116203b768ec23d710972abc0c5fb3
ismail baoud <ibaoud46@egmail.com>
2025-10-03 22:17:10 +0100

    Add PaymentStatus.java

---
+++ src/main/java/com/ismail/MicroCreditScoringSystem/model/enums/PaymentStatus.java
@@ -0,0 +1,10 @@
+package main.java.com.ismail.MicroCreditScoringSystem.model.enums;
+
+public enum PaymentStatus {
+    NON,
+    PAID_ON_TIME,
+    LATE,
+    PAID_LATE,
+    UNPAID_NOT_SETTLED,
+    UNPAID_SETTLED
+}
