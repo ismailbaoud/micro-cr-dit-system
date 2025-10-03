d4fe17f78ba23a7f0d05f4112f1ab1fa59cba194
ismail baoud <ibaoud46@egmail.com>
2025-10-03 22:17:10 +0100

    Add Menu.java

---
+++ src/main/java/com/ismail/MicroCreditScoringSystem/view/Menu.java
@@ -0,0 +1,45 @@
+package main.java.com.ismail.MicroCreditScoringSystem.view;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        Integer choice;
+        do {
+            System.out.println("1 => Client Management");
+            System.out.println("2 => Credit & Score Management");
+            System.out.println("3 => Payment Tracking");
+            System.out.println("4 => Decision Engine");
+            System.out.println("5 => Analytics & Reports");
+            System.out.println("0 => Exit");
+            System.out.print("Please enter your choice : ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1 :
+                    new ClientView().clientMenu();
+                    break;
+                case 2 :
+                    new CreditView().creditMenu();
+                    break;
+                case 3 :
+                    new PaymentTrackingMenu().paymentTrackingMenu();
+                    break;
+                case 4 :
+                    new DecisionEngineMenu().DecisionEngineMenu();
+                    break;
+                case 5 :
+                    new AnalyticsMenu().analyticsMenu();
+                    break;
+                case 0 :
+                    System.out.println("Good bye !");
+                    break;
+                default:
+                    System.out.println("\nInvalid choice please try valid choice !!\n");
+                    break;
+            }
+        }while (choice != 0);
+    }
+
+}
