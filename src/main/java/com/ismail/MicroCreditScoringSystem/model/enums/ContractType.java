f37b079153ed88a959776cc754925553c6dac4c0
ismail baoud <ibaoud46@egmail.com>
2025-10-03 22:17:10 +0100

    Add ContractType.java

---
+++ src/main/java/com/ismail/MicroCreditScoringSystem/model/enums/ContractType.java
@@ -0,0 +1,9 @@
+package main.java.com.ismail.MicroCreditScoringSystem.model.enums;
+
+public enum ContractType {
+    CDI ,
+    CDD,
+    Other,
+    STABLE_LIBERAL_PROFESSION,
+    SELF_EMPLOYED
+}
