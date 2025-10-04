0b8043d20a12f058a9d4c6d26cb79c91359fac57
ismail baoud <ibaoud46@egmail.com>
2025-10-04 18:59:40 +0100

    Update LoanView.java

--- src/main/java/com/ismail/MicroCreditScoringSystem/view/LoanView.java
+++ src/main/java/com/ismail/MicroCreditScoringSystem/view/LoanView.java
@@ -2,10 +2,12 @@ package main.java.com.ismail.MicroCreditScoringSystem.view;
 
 
 import main.java.com.ismail.MicroCreditScoringSystem.controller.LoanController;
+import main.java.com.ismail.MicroCreditScoringSystem.model.Loan;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CreditView {
+public class LoanView {
     //Calculate score automatically based on stability, financial capacity, history, etc.
     //
     //Check eligibility thresholds.
@@ -17,10 +19,19 @@ public class CreditView {
         do {
             System.out.println("1 => add new credit");
             System.out.println("2 => handle the manual review loans ");
-            System.out.println("3 => pay credit");
+            System.out.println("3 => display manual review loans ");
+            System.out.println("4 => display general statistics ");
+            System.out.println("5 => pay credit");
             System.out.println("0 => Go back");
             choice = scanner.nextInt();
             new LoanController(choice);
         }while (choice != 0);
+    }
+
+
+    public void displayLoans(ArrayList<Loan> loans) {
+        for(Loan loan : loans) {
+            System.out.println("Id : " + loan.getId() + " , Decision : "+loan.getDecision() + " , amount : " + loan.getMontantOctroye());
+        }
     }
 }
